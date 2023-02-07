53cdd6e47e22d7a5b4359fe7c0b6aa11173626f9
Yeshd <yeshodaprasai@gmail.com>
2023-02-07 14:37:43 -0500

    added subtract( ) To allow subtract.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -1,11 +1,14 @@
+#define _CRT_Secure_NO_WARNINGS
+
 #include <stdio.h>
 
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
-
-void main() {
+int main() 
+{
 
 	printWelcomeMenu();
 
@@ -13,15 +16,21 @@ void main() {
 
 	int inputNum;
 
-	printf("Enter operation number: ");
+	printf("Enter operation number:  ");
 	scanf_s("%1o", &inputNum);
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
+		
 	}
-
+	switch (inputNum)
+	{
+	case 2:
+		subtract();
+		
+	}
 }
 
 void printWelcomeMenu() {
@@ -44,6 +53,19 @@ void add() {
 	scanf_s("%lf", &num2);
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
-	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	result = num1 + num2+ num3;
+	printf("%lf + %lf +%lf= %lf\n", num1, num2, num3 ,result);
 }
+
+void subtract() {
+	double num1, num2, num3, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	printf("Enter the third value:");
+	scanf_s("%lf", &num3);
+	result = num1 - num2 - num3;
+	printf("%lf - %lf - %lf = %lf\n", num1, num2,num3,result);
+}
+
